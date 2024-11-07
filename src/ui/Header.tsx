52c881576e1e63a9b8f101141e351ddb0b0f19dd
Eun Oh <bdohhhhh@gmail.com>
2024-11-07 19:58:20 +0900

    [style]: header login 버튼 바운스 스타일 설정

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -61,7 +61,7 @@ function Header() {
           ) : (
             <button
               onClick={handleLogInClick}
-              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
+              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-bold rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 animate-bounce"
             >
               Log In
             </button>
