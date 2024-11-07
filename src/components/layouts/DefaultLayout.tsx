45a94b97dba3819fcbb2be5cb293a319d6c4d259
Eun Oh <bdohhhhh@gmail.com>
2024-11-07 19:03:30 +0900

    [style]: defaultLayout 수정

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -3,15 +3,15 @@ import { Header } from '../header';
 
 function DefaultLayout() {
   return (
-    <>
+    <section className="flex flex-col min-h-dvh">
       <Header />
       <main
         id="default-layout"
-        className="h-full grid place-items-center max-w-screen-lg mx-auto my-0 place-content-center"
+        className="h-full grid place-items-center max-w-screen-lg mx-auto my-0 place-content-center flex-1"
       >
         <Outlet />
       </main>
-    </>
+    </section>
   );
 }
 
