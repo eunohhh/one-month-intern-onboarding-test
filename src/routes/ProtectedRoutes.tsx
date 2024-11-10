a89c6fb570887e76fa04ca4d5156dfcde4457544
Eun Oh <bdohhhhh@gmail.com>
2024-11-10 22:03:28 +0900

    [update]: protected route 적용

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,13 +1,12 @@
-// import useAuth from '@/hooks/useAuth';
-import { Outlet } from 'react-router-dom';
+import useAuth from '@/auth/hooks';
+import { Navigate, Outlet } from 'react-router-dom';
 
 function ProtectedRoute() {
-  // const { isLoggedIn } = useAuth();
+  const { me } = useAuth();
 
-  // if (!isLoggedIn) {
-  //   // 유저 정보가 없다면 홈으로! 혹은 로그인페이지로 가게 할 수 있음
-  //   return <Navigate to="/" replace={true} />;
-  // }
+  if (!me) {
+    return <Navigate to="/" replace={true} />;
+  }
 
   // 유저 정보가 있다면 자식 컴포넌트를 보여줌
   return <Outlet />;
