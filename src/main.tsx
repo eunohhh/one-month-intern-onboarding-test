95cc7733e694dfbb4663f847901ff8e19295a17b
Eun Oh <bdohhhhh@gmail.com>
2024-11-10 22:47:48 +0900

    [script]: vercel analytics 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/react';
+import { inject } from '@vercel/analytics';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+inject();
 Sentry.init({
   dsn: 'https://d9cb8ccf5be4ad80ad0e242cf5f2fd6a@o4508274097717248.ingest.us.sentry.io/4508274100862976',
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
